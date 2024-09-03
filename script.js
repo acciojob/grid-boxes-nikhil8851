f05e6fe33d17bb0945aa98cf66e8b98c1cddc07d
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-03 12:11:48 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+describe('example to-do app', () => {
+    it('should have the correct grid layout for the container', () => {
+        cy.visit('http://localhost:3000');
+        cy.get('#grid', { timeout: 10000 }).should('have.css', 'display', 'grid');
+        cy.get('#grid').should('have.css', 'grid-template-rows', '124px 124px 124px 124px 124px');
+        cy.get('#grid').should('have.css', 'grid-template-columns', '242.5px 242.5px 242.5px 242.5px');
+        cy.get('#grid').should('have.css', 'grid-gap', '10px 10px');
+    });
+
+    it('should have correct layout for grid items', () => {
+        cy.visit('http://localhost:3000');
+        cy.get('#grid > div:nth-child(1)', { timeout: 10000 }).should('have.css', 'grid-column', 'span 4 / auto');
+        cy.get('#grid > div:nth-child(2)').should('have.css', 'grid-row', 'span 4 / auto');
+        cy.get('#grid > div:nth-child(4)').should('have.css', 'grid-column', 'span 2 / auto');
+        cy.get('#grid > div:nth-child(5)').should('have.css', 'grid-column', 'span 2 / auto');
+        cy.get('#grid > div:nth-child(10)').should('have.css', 'grid-column', 'span 3 / auto');
+    });
+});
